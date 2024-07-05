341a9b579a7456c4ab12adbb1c1e08cd94d891b1
ashish-cora <ashish1mah8@gmail.com>
2024-07-05 15:30:00 +0545

    menu MOdal now working

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -9,4 +9,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @ViewChild('menuIcon') menuIcon: ElementRef;
+  @ViewChild('closeIcon') closeIcon: ElementRef;
+  @ViewChild('menuModal') menuModal: ElementRef;
+
+  menuHandler(event: Event): void {
+    this.closeIcon.nativeElement.style.display = 'block';
+    this.menuIcon.nativeElement.style.display = 'none';
+    this.menuModal.nativeElement.style.right = ' 0px';
+    console.log('asdf00>>', this.menuModal.nativeElement.style.right);
+  }
+
+  closeHandler(event: Event): void {
+    this.menuIcon.nativeElement.style.display = 'block';
+    this.closeIcon.nativeElement.style.display = 'none';
+    this.menuModal.nativeElement.style.right = '-100%';
+  }
+}
